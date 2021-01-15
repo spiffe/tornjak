3695d01875571226c3767cea1660acc3037db111
Brandon Lum <lumjjb@gmail.com>
2021-01-15 16:10:50 -0500

    Added more agent APIs
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
     grpc "google.golang.org/grpc"
     "context"
 
+	types "github.com/spiffe/spire/proto/spire/types"
     agent "github.com/spiffe/spire/proto/spire/api/server/agent/v1"
 )
 
@@ -12,7 +13,6 @@ type ListAgentsRequest agent.ListAgentsRequest
 type ListAgentsResponse agent.ListAgentsResponse
 
 func (s *Server) ListAgents(inp ListAgentsRequest) (*ListAgentsResponse, error) {
-
     inpReq := agent.ListAgentsRequest(inp)
     var conn *grpc.ClientConn
 	conn, err := grpc.Dial(s.SpireServerAddr, grpc.WithInsecure())
@@ -30,8 +30,68 @@ func (s *Server) ListAgents(inp ListAgentsRequest) (*ListAgentsResponse, error) 
     return (*ListAgentsResponse)(resp), nil
 }
 
-/*
+type BanAgentRequest agent.BanAgentRequest 
 
+func (s *Server) BanAgent(inp BanAgentRequest) error {
+    inpReq := agent.BanAgentRequest(inp)
+    var conn *grpc.ClientConn
+	conn, err := grpc.Dial(s.SpireServerAddr, grpc.WithInsecure())
+	if err != nil {
+        return err
+	}
+	defer conn.Close()
+    client:= agent.NewAgentClient(conn)
+
+    _, err = client.BanAgent(context.Background(), &inpReq)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+type DeleteAgentRequest agent.DeleteAgentRequest 
+
+func (s *Server) DeleteAgent(inp DeleteAgentRequest) error {
+    inpReq := agent.DeleteAgentRequest(inp)
+    var conn *grpc.ClientConn
+	conn, err := grpc.Dial(s.SpireServerAddr, grpc.WithInsecure())
+	if err != nil {
+        return err
+	}
+	defer conn.Close()
+    client:= agent.NewAgentClient(conn)
+
+    _, err = client.DeleteAgent(context.Background(), &inpReq)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+type CreateJoinTokenRequest agent.CreateJoinTokenRequest 
+type CreateJoinTokenResponse types.JoinToken
+
+func (s *Server) CreateJoinToken(inp CreateJoinTokenRequest) (*CreateJoinTokenResponse, error) {
+    inpReq := agent.CreateJoinTokenRequest(inp)
+    var conn *grpc.ClientConn
+	conn, err := grpc.Dial(s.SpireServerAddr, grpc.WithInsecure())
+	if err != nil {
+        return nil, err
+	}
+	defer conn.Close()
+    client:= agent.NewAgentClient(conn)
+
+    joinToken, err := client.CreateJoinToken(context.Background(), &inpReq)
+    if err != nil {
+        return nil, err
+    }
+
+    return (*CreateJoinTokenResponse)(joinToken), nil
+}
+
+/*
 
 Agent 
 
--- api/server.go
+++ api/server.go
@@ -19,6 +19,8 @@ func (_ *Server) homePage(w http.ResponseWriter, r *http.Request){
 }
 
 func (s *Server) agentList (w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: Agent List")
+
     var input ListAgentsRequest
     buf := new(strings.Builder)
 
@@ -43,7 +45,7 @@ func (s *Server) agentList (w http.ResponseWriter, r *http.Request) {
 
     ret, err := s.ListAgents(input)
     if err != nil {
-        emsg := fmt.Sprintf("Error listing agents: %v", err.Error())
+        emsg := fmt.Sprintf("Error: %v", err.Error())
         http.Error(w, emsg, http.StatusBadRequest)
         return
     }
@@ -56,11 +58,134 @@ func (s *Server) agentList (w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Println("Endpoint Hit: Agent List")
 }
+
+
+
+func (s *Server) agentBan (w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: Agent Ban")
+
+    var input BanAgentRequest
+    buf := new(strings.Builder)
+
+    n, err := io.Copy(buf, r.Body)
+    if err != nil {
+        emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+    data := buf.String()
+
+    if n == 0 {
+        emsg := fmt.Sprintf("Error: no data provided")
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    } else {
+        err := json.Unmarshal([]byte(data), &input)
+        if err != nil {
+            emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+            http.Error(w, emsg, http.StatusBadRequest)
+            return
+        }
+    }
+
+    err = s.BanAgent(input)
+    if err != nil {
+        emsg := fmt.Sprintf("Error listing agents: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+
+    w.Write([]byte("SUCCESS"))
+}
+
+func (s *Server) agentDelete (w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: Agent Delete")
+
+    var input DeleteAgentRequest
+    buf := new(strings.Builder)
+
+    n, err := io.Copy(buf, r.Body)
+    if err != nil {
+        emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+    data := buf.String()
+
+    if n == 0 {
+        emsg := fmt.Sprintf("Error: no data provided")
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    } else {
+        err := json.Unmarshal([]byte(data), &input)
+        if err != nil {
+            emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+            http.Error(w, emsg, http.StatusBadRequest)
+            return
+        }
+    }
+
+    err = s.DeleteAgent(input)
+    if err != nil {
+        emsg := fmt.Sprintf("Error listing agents: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+
+    w.Write([]byte("SUCCESS"))
+}
+
+func (s *Server) agentCreateJoinToken (w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: Agent Create Join Token")
+
+    var input CreateJoinTokenRequest
+    buf := new(strings.Builder)
+
+    n, err := io.Copy(buf, r.Body)
+    if err != nil {
+        emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+    data := buf.String()
+
+    if n == 0 {
+        input = CreateJoinTokenRequest{}
+    } else {
+        err := json.Unmarshal([]byte(data), &input)
+        if err != nil {
+            emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
+            http.Error(w, emsg, http.StatusBadRequest)
+            return
+        }
+    }
+
+    ret, err := s.CreateJoinToken(input)
+    if err != nil {
+        emsg := fmt.Sprintf("Error: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+
+    je := json.NewEncoder(w)
+    err = je.Encode(ret)
+    if err != nil {
+        emsg := fmt.Sprintf("Error encoding output: %v", err.Error())
+        http.Error(w, emsg, http.StatusBadRequest)
+        return
+    }
+
+}
+
+
+
 
 func (s *Server) HandleRequests() {
     http.HandleFunc("/", s.homePage)
     http.HandleFunc("/agent/list", s.agentList)
+    http.HandleFunc("/agent/ban", s.agentBan)
+    http.HandleFunc("/agent/delete", s.agentDelete)
+    http.HandleFunc("/agent/createjointoken", s.agentCreateJoinToken)
     log.Fatal(http.ListenAndServe(":10000", nil))
 }
