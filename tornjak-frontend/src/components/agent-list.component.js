fa002f41bc8a854d9c1311586c97901b1b0735ad
Brandon Lum <lumjjb@gmail.com>
2021-02-15 14:21:41 -0500

    Add manager capabilities to agent-list
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- tornjak-frontend/src/components/agent-list.component.js
+++ tornjak-frontend/src/components/agent-list.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import GetApiServerUri from './helpers';
+import IsManager from './is_manager';
 
 const Agent = props => (
   <tr>
@@ -22,18 +23,60 @@ const Agent = props => (
   </tr>
 )
 
+const ServerDropdown = props => (
+    <option value={props.value}>{props.name}</option>
+)
+
 export default class AgentList extends Component {
   constructor(props) {
     super(props);
     this.deleteAgent = this.deleteAgent.bind(this);
     this.banAgent = this.banAgent.bind(this);
-    this.state = { agents: [] };
+    this.serverDropdownList = this.serverDropdownList.bind(this);
+    this.onServerSelect = this.onServerSelect.bind(this);
+    this.state = { 
+        agents: [],
+        servers: [],
+        message: "",
+    };
   }
 
   componentDidMount() {
+      if (IsManager) {
+        this.populateServers()
+      } else {
+        this.populateLocalAgents()
+      }
+  }
+
+  populateServers () {
+    axios.get(GetApiServerUri("/manager-api/server/list"), { crossdomain: true })
+      .then(response => {
+        this.setState({ servers:response.data["servers"]} );
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  populateAgents(serverName) {
+      axios.get(GetApiServerUri('/manager-api/agent/list/') + serverName, {     crossdomain: true })
+      .then(response =>{
+        console.log(response);
+        this.setState({ agents:response.data["agents"]});
+      }).catch(error => {
+          this.setState({
+              message: "Error retrieving " + serverName + " : "+ error.message,
+              agents: [],
+          });
+      });
+
+  }
+
+  populateLocalAgents() {
     axios.get(GetApiServerUri('/api/agent/list'), { crossdomain: true })
       .then(response => {
-        this.setState({ agents:response.data["agents"]} );
+        this.setState({ agents:response.data["agents"]});
       })
       .catch((error) => {
         console.log(error);
@@ -82,10 +125,64 @@ export default class AgentList extends Component {
     }
   }
 
+  serverDropdownList() {
+    if (typeof this.state.servers !== 'undefined') {
+        return this.state.servers.map(server => {
+          return <ServerDropdown key={server.name}
+                    value={server.name}
+                    name={server.name} />
+        })
+    } else {
+        return ""
+    }
+  }
+
+  onServerSelect(e) {
+      const serverName = e.target.value;
+      if (serverName !== "") {
+          this.populateAgents(serverName)
+      }
+  }
+
+  getServer(serverName) {
+      var i;
+      const servers = this.state.servers
+      for (i = 0; i < servers.length; i++) {
+        if (servers[i].name === serverName) {
+            return servers[i]
+        }
+      }
+      return null
+  }
+
+
+
   render() {
+
+    let managerServerSelector =  (
+        <div id="server-dropdown-div">
+        <label id="server-dropdown">Choose a server:</label>
+        <br/>
+        <select name="servers" id="servers" onChange={this.onServerSelect}>
+          <optgroup label="Servers">
+            <option value=""/>
+                {this.serverDropdownList()}
+          </optgroup>
+        </select>
+        </div>
+    )
+
     return (
       <div>
         <h3>Agent List</h3>
+        <div className="alert alert-primary" role="alert">
+        <pre>
+           {this.state.message}
+        </pre>
+        </div>
+        {IsManager && managerServerSelector}
+        <br/><br/>
+
         <table className="table" style={{width : "100%"}}>
           <thead className="thead-light">
             <tr>
