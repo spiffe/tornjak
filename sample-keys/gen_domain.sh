a9534557d6900e0d478b16f0e20f773a3f84c245
Brandon Lum <lumjjb@gmail.com>
2021-03-02 15:18:17 -0500

    Removed creation of k8s secrets in gen script
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- sample-keys/gen_domain.sh
+++ sample-keys/gen_domain.sh
@@ -10,7 +10,7 @@ export DOMAIN=$1
 echo "Generating certs..."
 openssl req -new -x509 -sha256 -key key.pem -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${DOMAIN},DNS:example.com,DNS:www.example.com")) -out $DOMAIN.pem
 
-echo "Creating k8s secrets in tornjak namesapce..."
-kubectl -n tornjak create secret generic tornjak-certs --from-file=key.pem --from-file=cert.pem=$DOMAIN.pem
+#echo "Creating k8s secrets in tornjak namesapce..."
+#kubectl -n tornjak create secret generic tornjak-certs --from-file=key.pem --from-file=cert.pem=$DOMAIN.pem
 
 # Modify tornjak server k8s manifest to add Volume from secret "tornjak-certs" to mount point "/opt/spire/sample-keys"
