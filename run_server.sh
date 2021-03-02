221cfd53615841956eefdcb20c416a8a23b34f71
Brandon Lum <lumjjb@gmail.com>
2021-03-02 15:26:35 -0500

    Set output of gen_domain.sh to out.pem
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "${@}"
 /opt/spire/tornjak-agent -c $2 http &
-/opt/spire/tornjak-agent -c /run/spire/config/server.conf http --tls --cert sample-keys/cert.pem --key sample-keys/key.pem  --listen-addr :20000 &
-/opt/spire/tornjak-agent -c /run/spire/config/server.conf http --mtls --cert sample-keys/cert.pem --key sample-keys/key.pem  --listen-addr :30000 &
+/opt/spire/tornjak-agent -c /run/spire/config/server.conf http --tls --cert sample-keys/tls.pem --key sample-keys/key.pem  --listen-addr :20000 &
+/opt/spire/tornjak-agent -c /run/spire/config/server.conf http --mtls --cert sample-keys/mtls.pem --key sample-keys/key.pem  --listen-addr :30000 &
 # PORT=3000 npm start --prefix ./tornjak-frontend &
 /usr/bin/dumb-init /opt/spire/bin/spire-server run "${@}" 
--- sample-keys/gen_domain.sh
+++ sample-keys/gen_domain.sh
@@ -8,7 +8,7 @@ function usage {
 export DOMAIN=$1
 
 echo "Generating certs..."
-openssl req -new -x509 -sha256 -key key.pem -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${DOMAIN},DNS:example.com,DNS:www.example.com")) -out $DOMAIN.pem
+openssl req -new -x509 -sha256 -key key.pem -subj "/C=US/ST=CA/O=Acme, Inc./CN=example.com" -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${DOMAIN},DNS:example.com,DNS:www.example.com")) -out out.pem
 
 #echo "Creating k8s secrets in tornjak namesapce..."
 #kubectl -n tornjak create secret generic tornjak-certs --from-file=key.pem --from-file=cert.pem=$DOMAIN.pem
