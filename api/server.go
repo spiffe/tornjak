0967fece0ac0e5058ca0e9f6616184daf2adda16
Brandon Lum <lumjjb@gmail.com>
2021-04-06 10:32:32 -0400

    Fil deep link 404 issues
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- api/server.go
+++ api/server.go
@@ -5,10 +5,13 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -360,25 +363,68 @@ func (s *Server) getTornjakServerInfo(w http.ResponseWriter, r *http.Request) {
 	je.Encode(ret)
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func (s *Server) HandleRequests() {
+	rtr := mux.NewRouter()
 
 	// Agents
-	http.HandleFunc("/api/agent/list", corsHandler(s.agentList))
-	http.HandleFunc("/api/agent/ban", corsHandler(s.agentBan))
-	http.HandleFunc("/api/agent/delete", corsHandler(s.agentDelete))
-	http.HandleFunc("/api/agent/createjointoken", corsHandler(s.agentCreateJoinToken))
+	rtr.HandleFunc("/api/agent/list", corsHandler(s.agentList))
+	rtr.HandleFunc("/api/agent/ban", corsHandler(s.agentBan))
+	rtr.HandleFunc("/api/agent/delete", corsHandler(s.agentDelete))
+	rtr.HandleFunc("/api/agent/createjointoken", corsHandler(s.agentCreateJoinToken))
 
 	// Entries
-	http.HandleFunc("/api/entry/list", corsHandler(s.entryList))
-	http.HandleFunc("/api/entry/create", corsHandler(s.entryCreate))
-	http.HandleFunc("/api/entry/delete", corsHandler(s.entryDelete))
+	rtr.HandleFunc("/api/entry/list", corsHandler(s.entryList))
+	rtr.HandleFunc("/api/entry/create", corsHandler(s.entryCreate))
+	rtr.HandleFunc("/api/entry/delete", corsHandler(s.entryDelete))
 
 	// Tornjak specific
-	http.HandleFunc("/api/tornjak/serverinfo", corsHandler(s.getTornjakServerInfo))
+	rtr.HandleFunc("/api/tornjak/serverinfo", corsHandler(s.getTornjakServerInfo))
 
 	// UI
-	//http.HandleFunc("/", s.homePage)
-	http.Handle("/", http.FileServer(http.Dir("./ui-agent")))
+	spa := spaHandler{staticPath: "ui-agent", indexPath: "index.html"}
+	rtr.PathPrefix("/").Handler(spa)
 
 	// TLS Stack handling
 	if s.TlsEnabled || s.MTlsEnabled {
@@ -403,6 +449,7 @@ func (s *Server) HandleRequests() {
 
 		// Create a Server instance to listen on port 8443 with the TLS config
 		server := &http.Server{
+			Handler:   rtr,
 			Addr:      s.ListenAddr,
 			TLSConfig: tlsConfig,
 		}
@@ -412,6 +459,6 @@ func (s *Server) HandleRequests() {
 		return
 	} else {
 		fmt.Printf("Starting to listen on %s...\n", s.ListenAddr)
-		log.Fatal(http.ListenAndServe(s.ListenAddr, nil))
+		log.Fatal(http.ListenAndServe(s.ListenAddr, rtr))
 	}
 }
