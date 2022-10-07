cf28ac57b4624df1240a2773a00be70c8b2c2704
Aura6 <aura1391215@gmail.com>
2022-10-07 15:30:57 -0400

    another comment
    Signed-off-by: Aura6 <aura1391215@gmail.com>

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "${@}"
+# comment
 /opt/spire/tornjak-agent -c $2 http &
 /opt/spire/tornjak-agent -c /run/spire/config/server.conf http --tls --cert sample-keys/tls.pem --key sample-keys/key.pem  --listen-addr :20000 &
 /opt/spire/tornjak-agent -c /run/spire/config/server.conf http --mtls --cert sample-keys/tls.pem --key sample-keys/key.pem  --mtls-ca sample-keys/rootCA.pem --listen-addr :30000 &
