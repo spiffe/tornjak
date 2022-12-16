21286f4ba509ed340f30fd070564852fbd45080a
Aura6 <aura1391215@gmail.com>
2022-12-16 14:24:18 -0500

    comments
    Signed-off-by: Aura6 <aura1391215@gmail.com>

--- tornjak-frontend/src/components/__tests__/cluster-list.test.jsx
+++ tornjak-frontend/src/components/__tests__/cluster-list.test.jsx
@@ -1,5 +1,5 @@
 import { ClusterList } from "../../components/cluster-list"
-import { findByTestId, checkProps } from "../../Utils/index"
+import { checkProps, findByTestId } from "../../Utils/index"
 import { shallow, configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 import * as IsManager from "../../components/is_manager"
@@ -7,48 +7,54 @@ import * as IsManager from "../../components/is_manager"
 configure({ adapter: new Adapter() })
 
 const props = {
-    clustersListUpdateFunc: jest.fn(), 
-    tornjakMessageFunc: jest.fn(), 
-    serverInfoUpdateFunc: jest.fn(), 
-    globalServerSelected: "Test String",
-    globalErrorMessage: "Test String", 
-    globalTornjakServerInfo: {}, 
-    globalClustersList: [{}]
-  };
+  clustersListUpdateFunc: jest.fn(), 
+  tornjakMessageFunc: jest.fn(), 
+  serverInfoUpdateFunc: jest.fn(), 
+  globalServerSelected: "Test String",
+  globalErrorMessage: "Test String", 
+  globalTornjakServerInfo: {}, 
+  globalClustersList: [{}]
+};
 
 describe("Cluster List Component", () => {
 
   describe("Checking PropTypes", () => {
 
+    // Props are allowed to be undefined/missing.
     test("Should NOT throw Warning/ Error", () => {
-      const expectedInitialProps = props;
-      const propsErr = checkProps(ClusterList, expectedInitialProps);
+      const propsErr = checkProps(ClusterList, props);
       expect(propsErr).toBeUndefined();
     })
   })
 
   describe("Should Render Properly", () => {
+    // Creates a rendered copy of a ClusterList.
     const wrapper = shallow(<ClusterList {...props} />)
     
     it("Should Render", () => {
+      // Generates html as a point of comparison.
       expect(wrapper).toMatchSnapshot()
     })
 
+    it("Should Render Cluster Management Component without Errors", () => {
+      // Locates the list element containing the clusters (should be empty at start).
+      const clusterMan = findByTestId(wrapper, "cluster-management");
+      expect(clusterMan.length).toBe(0);
+    });
+
     it("Undefined Cluster List", () => {
+      // Renders with different props to ensure the project works with multiple parameters.
       const newProps = {...props}
       newProps.globalClustersList = undefined
       shallow(<ClusterList {...newProps} />)
     })
 
     describe("Methods", () => {
-  
-      beforeEach(() => {
-        jest.resetModules()
-      });
 
       describe("Mounts", () => {
         
         test("With Manager", () => {
+          // Renders with user management enabled.
           IsManager.default = true
           shallow(<ClusterList {...props} />)
         })
