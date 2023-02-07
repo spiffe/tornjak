627901a080bdb4ecf75f00ad300988d2e285ae2c
Maia Iyer <maia.raj.iyer@gmail.com>
2023-02-07 15:04:58 -0500

    small edit of documentation
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- run_backend.sh
+++ run_backend.sh
@@ -7,7 +7,7 @@ Usage: run_server [-c <file>] [-t <file>]
 
 -c | -config <file>: SPIRE Config File
 -t | -tornjak-config <file>: Tornjak Config File
--expandEnv | expand environment variables expressed in Tornjak Config File
+-expandEnv : if flag included, expand environment variables expressed in Tornjak Config File
 EOF
 	exit 1
 }
