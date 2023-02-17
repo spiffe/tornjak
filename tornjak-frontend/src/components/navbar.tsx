d819169a50120e9adadce4af71ffa0071030211b
Mohammed Abdi <48661743+mamy-CS@users.noreply.github.com>
2023-02-17 18:34:36 +0000

    No IAM - Provide full access (#121)
    * no IAM full access
    
    Signed-off-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>
    
    * print isadmin and withauth
    
    Signed-off-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>
    
    * fix without auth bug
    
    Signed-off-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>
    
    * nit delete comments
    
    Signed-off-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>
    
    ---------
    
    Signed-off-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>
    Co-authored-by: Mohammed Abdi <moabdi@Mohammeds-MBP-2.attlocal.net>

--- tornjak-frontend/src/components/navbar.tsx
+++ tornjak-frontend/src/components/navbar.tsx
@@ -73,7 +73,6 @@ class NavigationBar extends Component<NavigationBarProp, NavigationBarState> {
       <div className="dropdown">
         <a href="/server/manage" className="dropbtn">Manage Servers</a>
       </div>
-
     return (
       <div data-test="nav-bar">
         <div className="navigation-bar">
@@ -82,7 +81,7 @@ class NavigationBar extends Component<NavigationBarProp, NavigationBarState> {
               <a href="/clusters" className="dropbtn">Clusters </a>
               <div className="dropdown-content">
                 <a href="/clusters" className="nav-link">Clusters List</a>
-                {(isAdmin || (withAuth === undefined)) &&
+                {(isAdmin || !withAuth) &&
                   <a href="/cluster/clustermanagement" className="nav-link">Cluster Management</a>
                 }
               </div>
@@ -91,7 +90,7 @@ class NavigationBar extends Component<NavigationBarProp, NavigationBarState> {
               <a href="/agents" className="dropbtn">Agents </a>
               <div className="dropdown-content">
                 <a href="/agents" className="nav-link">Agents List</a>
-                {(isAdmin || (withAuth === undefined)) &&
+                {(isAdmin || !withAuth) &&
                   <a href="/agent/createjointoken" className="nav-link">Create Token</a>
                 }
               </div>
@@ -100,7 +99,7 @@ class NavigationBar extends Component<NavigationBarProp, NavigationBarState> {
               <a href="/entries" className="dropbtn">Entries</a>
               <div className="dropdown-content">
                 <a href="/entries" className="nav-link">Entries List</a>
-                {(isAdmin || (withAuth === undefined)) &&
+                {(isAdmin || !withAuth) &&
                   <a href="/entry/create" className="nav-link">Create Entries</a>
                 }
               </div>
