2939837a56d51af8f66da4517dfd502f1c82ff70
Maia Iyer <maia.raj.iyer@gmail.com>
2023-05-01 22:44:30 -0400

    Shrinking run scripts
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- run_backend.sh
+++ run_backend.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 echo "${@}"
 
-/opt/spire/tornjak-backend ${@} serverinfo &
-#/opt/spire/tornjak-backend ${@} http --tls --cert sample-keys/tls.pem --key sample-keys/key.pem  --listen-addr :20000 &
-#/opt/spire/tornjak-backend ${@} http --mtls --cert sample-keys/tls.pem --key sample-keys/key.pem  --mtls-ca sample-keys/rootCA.pem --listen-addr :30000 &
+/opt/spire/tornjak-backend ${@} serverinfo
 /opt/spire/tornjak-backend ${@} http
