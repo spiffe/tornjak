4b4a1890f183e1c56ca08c2ff63a97c03b1be7bd
Maia Iyer <maia.raj.iyer@gmail.com>
2023-05-03 10:36:23 -0400

    Added script comments
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- run_tornjak.sh
+++ run_tornjak.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 echo "${@}"
 
+# run frontend in background
 npm start &
 
+# run serverinfo to print SPIRE config if given and Tornjak config
 /opt/spire/tornjak-backend ${@} serverinfo
-/opt/spire/tornjak-backend ${@} http 
+
+# run Tornjak server
+/opt/spire/tornjak-backend ${@} http
