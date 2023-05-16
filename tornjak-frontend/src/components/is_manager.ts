5cee8b2f70ec7ffaf22112ce5c26766260c03522
Mohammed Abdi <mohammma@usc.edu>
2023-05-16 16:26:18 -0400

    dynamically change frontend env variables
    Signed-off-by: Mohammed Abdi <mohammma@usc.edu>

--- tornjak-frontend/src/components/is_manager.ts
+++ tornjak-frontend/src/components/is_manager.ts
@@ -1,5 +1,7 @@
+import {env} from '../env';
+
 // Is Manager
-console.log("IS_MANAGER:" + process.env.REACT_APP_TORNJAK_MANAGER)
-const IsManager = process.env.REACT_APP_TORNJAK_MANAGER !== undefined && process.env.REACT_APP_TORNJAK_MANAGER.toUpperCase() !== "FALSE"
+console.log("IS_MANAGER:" + env.REACT_APP_TORNJAK_MANAGER)
+const IsManager = env.REACT_APP_TORNJAK_MANAGER !== undefined && env.REACT_APP_TORNJAK_MANAGER.toUpperCase() !== "FALSE"
 
 export default IsManager
