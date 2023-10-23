6e7c0336a3fb428d16c811f1507d9b6dfb25936c
Maia Iyer <maia.raj.iyer@gmail.com>
2023-10-23 19:19:12 -0400

    Moved golang files up a directory
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

---
+++ pkg/manager/auth/no_auth.go
@@ -0,0 +1,13 @@
+package auth
+
+import "net/http"
+
+type NullVerifier struct {}
+
+func NewNullVerifier() (*NullVerifier) {
+	return &NullVerifier {}
+}
+
+func (v *NullVerifier) Verify(r *http.Request) error {
+	return nil
+}
