f7c674e8ac60cdd1f918383c7a16cdf269406d8f
Xuhang Cao <ccao@oberlin.edu>
2024-08-30 15:11:25 -0400

    api v1 mapping
    Signed-off-by: Xuhang Cao <ccao@oberlin.edu>

--- pkg/agent/authorization/rbac.go
+++ pkg/agent/authorization/rbac.go
@@ -34,8 +34,22 @@ var staticAPIList = map[string]struct{}{
 	"/api/tornjak/clusters/edit":      {},
 	"/api/tornjak/clusters/delete":    {},
 }
+var staticAPIV1List = map[string]map[string]struct{}{
+	"/api/v1/spire/serverinfo" :{"GET": {}},
+	"/api/v1/spire/healthcheck" :{"GET": {}},
+	"/api/v1/spire/entries" :{"GET": {}, "POST": {}, "DELETE": {}},
+	"/api/v1/spire/agents" :{"GET": {}, "POST": {}, "DELETE": {}},
+	"/api/v1/spire/agent/ban" :{"POST": {}},
+	"/api/v1/spire/agents/jointoken" :{"POST": {}},
+	"/api/v1/tornjak/clusters" :{"GET": {}, "POST": {}, "PATCH": {}, "DELETE": {}},
+	"/api/v1/tornjak/selectors" :{"GET": {}, "POST": {}},
+	"/api/v1/tornjak/agents" :{"GET": {}},
+	"/api/v1/tornjak/serverinfo" :{"GET": {}},
+	"/api/v1/spire/bundle" :{"GET": {}},
+	"/api/v1/spire/federations/bundles" :{"GET": {}, "POST": {}, "DELETE": {}, "PATCH": {}},
+}
 
-func validateInitParameters(roleList map[string]string, apiMapping map[string][]string) error {
+func validateInitParameters(roleList map[string]string, apiMapping map[string][]string, apiV1Mapping map[string]map[string][]string) error {
 	for api, allowList := range apiMapping {
 		// check that API exists
 		if _, ok := staticAPIList[api]; !ok {
@@ -49,11 +63,26 @@ func validateInitParameters(roleList map[string]string, apiMapping map[string][]
 			}
 		}
 	}
+	for path, method_dict := range apiV1Mapping {
+		for method, allowList := range method_dict {
+			// check that API exists
+			if _, ok := staticAPIV1List[path][method]; !ok {
+				return errors.Errorf("API path %s does not exist with method %s", path, method)
+			}
+
+			// check that each role exists in roleList
+			for _, allowedRole := range allowList {
+				if _, ok := roleList[allowedRole]; !ok {
+					return errors.Errorf("API %s lists undefined role %s", path, allowedRole)
+				}
+			}
+		}
+	}
 	return nil
 }
 
 func NewRBACAuthorizer(policyName string, roleList map[string]string, apiMapping map[string][]string, apiV1Mapping map[string]map[string][]string) (*RBACAuthorizer, error) {
-	err := validateInitParameters(roleList, apiMapping)
+	err := validateInitParameters(roleList, apiMapping, apiV1Mapping)
 	if err != nil {
 		return nil, errors.Errorf("Could not parse policy %s: invalid mapping: %v", policyName, err)
 	}
