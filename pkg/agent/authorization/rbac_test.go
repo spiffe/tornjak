7322b2f3a69b36b71c87c17dc146b6161ce6a4de
Xuhang Cao <ccao@oberlin.edu>
2024-09-03 18:32:29 -0400

    Start writing rbac tests
    Signed-off-by: Xuhang Cao <ccao@oberlin.edu>

--- pkg/agent/authorization/rbac_test.go
+++ pkg/agent/authorization/rbac_test.go
@@ -23,6 +23,13 @@ func TestNewRBACAuthorizer(t *testing.T) {
 
 	roleList_3 := map[string]string{"admin": "admin", "user": "user"}
 	apiV1Mapping_3 := apiV1Mapping_1
+
+	roleList_4 := roleList_2
+	apiV1Mapping_4 := map[string]map[string][]string{"/api/v1/unknown/serverinfo": {"GET": {"admin", "viewer"}}}
+
+	roleList_5 := roleList_2
+	apiV1Mapping_5 := map[string]map[string][]string{"/api/v1/spire/serverinfo": {"POST": {"admin", "viewer"}}}
+
 	// fail when roles in apiMapping not in roleList
 	_, err = NewRBACAuthorizer(policyName, roleList_1, nil, apiV1Mapping_1)
 	expectedErr := "Could not parse policy testPolicy: invalid mapping: API V1  /api/v1/spire/serverinfo lists undefined role viewer"
@@ -39,10 +46,31 @@ func TestNewRBACAuthorizer(t *testing.T) {
 	}
 
 	// fail when typo in apiMapping
+	_, err = NewRBACAuthorizer(policyName, roleList_3, nil, apiV1Mapping_3)
+	expectedErr = "Could not parse policy testPolicy: invalid mapping: API V1  /api/v1/spire/serverinfo lists undefined role viewer"
+	if err == nil {
+		t.Fatal("ERROR: successfully initialized RBAC without roles")
+	} else if err.Error() != expectedErr {
+		t.Fatalf("ERROR: expected %s, got %s", expectedErr, err.Error())
+	}
 
 	// fail when apiV1Mapping has path not in staticAPIV1List
+	_, err = NewRBACAuthorizer(policyName, roleList_4, nil, apiV1Mapping_4)
+	expectedErr = "Could not parse policy testPolicy: invalid mapping: API V1 path /api/v1/unknown/serverinfo does not exist with method GET"
+	if err == nil {
+		t.Fatal("ERROR: successfully initialized RBAC without roles")
+	} else if err.Error() != expectedErr {
+		t.Fatalf("ERROR: expected %s, got %s", expectedErr, err.Error())
+	}
 
 	// fail when apiV1Mapping has method not in staticAPIV1List
+	_, err = NewRBACAuthorizer(policyName, roleList_5, nil, apiV1Mapping_5)
+	expectedErr = "Could not parse policy testPolicy: invalid mapping: API V1 path /api/v1/spire/serverinfo does not exist with method POST"
+	if err == nil {
+		t.Fatal("ERROR: successfully initialized RBAC without roles")
+	} else if err.Error() != expectedErr {
+		t.Fatalf("ERROR: expected %s, got %s", expectedErr, err.Error())
+	}
 
 }
 // func TestAuthorizeRequest(t *testing.T) {
