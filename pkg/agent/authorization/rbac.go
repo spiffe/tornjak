4284f92665e01fc8b31a89ba0973c1cad1cffcad
Maia Iyer <maia.raj.iyer@gmail.com>
2024-10-07 12:14:22 -0400

    Remove old api logic from RBAC code
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- pkg/agent/authorization/rbac.go
+++ pkg/agent/authorization/rbac.go
@@ -12,31 +12,10 @@ import (
 type RBACAuthorizer struct {
 	name       string
 	roleList   map[string]string
-	apiMapping map[string][]string
 	apiV1Mapping map[string]map[string][]string
 }
 
 // TODO put this in a common constants file
-var staticAPIList = map[string]struct{}{
-	"/":                               {},
-	"/api/healthcheck":                {},
-	"/api/debugserver":                {},
-	"/api/agent/list":                 {},
-	"/api/entry/list":                 {},
-	"/api/tornjak/serverinfo":         {},
-	"/api/tornjak/selectors/list":     {},
-	"/api/tornjak/agents/list":        {},
-	"/api/tornjak/clusters/list":      {},
-	"/api/agent/ban":                  {},
-	"/api/agent/delete":               {},
-	"/api/agent/createjointoken":      {},
-	"/api/entry/create":               {},
-	"/api/entry/delete":               {},
-	"/api/tornjak/selectors/register": {},
-	"/api/tornjak/clusters/create":    {},
-	"/api/tornjak/clusters/edit":      {},
-	"/api/tornjak/clusters/delete":    {},
-}
 var staticAPIV1List = map[string]map[string]struct{}{
 	"/api/v1/spire/serverinfo" :{"GET": {}},
 	"/api/v1/spire/healthcheck" :{"GET": {}},
@@ -52,22 +31,9 @@ var staticAPIV1List = map[string]map[string]struct{}{
 	"/api/v1/spire/federations/bundles" :{"GET": {}, "POST": {}, "DELETE": {}, "PATCH": {}},
 }
 
-func validateInitParameters(roleList map[string]string, apiMapping map[string][]string, apiV1Mapping map[string]map[string][]string) error {
+func validateInitParameters(roleList map[string]string, apiV1Mapping map[string]map[string][]string) error {
 	if roleList == nil {
 		return errors.Errorf("No roles defined")
-	}
-	for api, allowList := range apiMapping {
-		// check that API exists
-		if _, ok := staticAPIList[api]; !ok {
-			return errors.Errorf("API %s does not exist", api)
-		}
-
-		// check that each role exists in roleList
-		for _, allowedRole := range allowList {
-			if _, ok := roleList[allowedRole]; !ok {
-				return errors.Errorf("API %s lists undefined role %s", api, allowedRole)
-			}
-		}
 	}
 	for path, method_dict := range apiV1Mapping {
 		for method, allowList := range method_dict {
@@ -87,44 +53,16 @@ func validateInitParameters(roleList map[string]string, apiMapping map[string][]
 	return nil
 }
 
-func NewRBACAuthorizer(policyName string, roleList map[string]string, apiMapping map[string][]string, apiV1Mapping map[string]map[string][]string) (*RBACAuthorizer, error) {
-	err := validateInitParameters(roleList, apiMapping, apiV1Mapping)
+func NewRBACAuthorizer(policyName string, roleList map[string]string, apiV1Mapping map[string]map[string][]string) (*RBACAuthorizer, error) {
+	err := validateInitParameters(roleList, apiV1Mapping)
 	if err != nil {
 		return nil, errors.Errorf("Could not parse policy %s: invalid mapping: %v", policyName, err)
 	}
-	fmt.Printf("apiV1Mapping: %v\n", apiV1Mapping)
 	return &RBACAuthorizer{
 		name:       policyName,
 		roleList:   roleList,
-		apiMapping: apiMapping,
 		apiV1Mapping: apiV1Mapping,
 	}, nil
-}
-
-func (a *RBACAuthorizer) authorizeAPIRequest(r *http.Request, u *user.UserInfo) error {
-	userRoles := u.Roles
-	apiPath := r.URL.Path
-
-	allowedRoles := a.apiMapping[apiPath]
-
-	// if no role listed for api, reject
-	if len(allowedRoles) == 0 {
-		return errors.New("Unauthorized request")
-	}
-
-	// check each allowed role
-	for _, allowedRole := range allowedRoles {
-		if allowedRole == "" { // all authenticated allowed
-			return nil
-		}
-		for _, role := range userRoles {
-			// user has role
-			if role == allowedRole {
-				return nil
-			}
-		}
-	}
-	return errors.New("Unauthorized Request")
 }
 
 func (a *RBACAuthorizer) authorizeAPIV1Request(r *http.Request, u *user.UserInfo) error {
@@ -162,20 +100,10 @@ func (a *RBACAuthorizer) AuthorizeRequest(r *http.Request, u *user.UserInfo) err
 
 	// based on path
 	apiPath := r.URL.Path
-	isV1 := strings.HasPrefix(apiPath, "/api/v1")
-
-	if !isV1 {
-		// check old API Request
-		err := a.authorizeAPIRequest(r, u)
-		if err != nil {
-			return errors.Errorf("Tornjak API Authorization error: %v", err)
-		}
-	} else {
-		// check API V1 Request
-    err := a.authorizeAPIV1Request(r, u)
-		if err != nil {
-			return errors.Errorf("Tornjak API V1 Authorization error: %v", err)
-		}
+	// check API V1 Request
+  err := a.authorizeAPIV1Request(r, u)
+	if err != nil {
+		return errors.Errorf("Tornjak API V1 Authorization error: %v", err)
 	}
 
 	return nil
