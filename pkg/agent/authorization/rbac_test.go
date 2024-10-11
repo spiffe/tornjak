f31c6e3cf54ad434440a397a4b59a6331b97f761
Maia Iyer <maia.raj.iyer@gmail.com>
2024-10-11 16:17:06 -0400

    Deprecate Old API (#534)
    * Remove old api logic from RBAC code
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * removed handlers
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * updated api docs
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * updated quickstart
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * fix build
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * removed types
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit spacing fixes
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Nits
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    ---------
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- pkg/agent/authorization/rbac_test.go
+++ pkg/agent/authorization/rbac_test.go
@@ -9,7 +9,7 @@ import (
 func TestNewRBACAuthorizer(t *testing.T) {
 	// INIT failures
 	// fail when no roles defined
-	_, err := NewRBACAuthorizer("", nil, nil, nil)
+	_, err := NewRBACAuthorizer("", nil, nil)
 	if err == nil {
 		t.Fatal("ERROR: successfully initialized RBAC without roles")
 	}
@@ -32,7 +32,7 @@ func TestNewRBACAuthorizer(t *testing.T) {
 	apiV1Mapping_5 := map[string]map[string][]string{"/api/v1/spire/serverinfo": {"POST": {"admin", "viewer"}}}
 
 	// fail when roles in apiMapping not in roleList
-	_, err = NewRBACAuthorizer(policyName, roleList_1, nil, apiV1Mapping_1)
+	_, err = NewRBACAuthorizer(policyName, roleList_1, apiV1Mapping_1)
 	expectedErr := "Could not parse policy testPolicy: invalid mapping: API V1  /api/v1/spire/serverinfo lists undefined role viewer"
 	if err == nil {
 		t.Fatal("ERROR: successfully initialized RBAC without roles")
@@ -41,13 +41,13 @@ func TestNewRBACAuthorizer(t *testing.T) {
 	}
 
 	// pass when roles in apiMapping in roleList
-	_, err = NewRBACAuthorizer(policyName, roleList_2, nil, apiV1Mapping_2)
+	_, err = NewRBACAuthorizer(policyName, roleList_2, apiV1Mapping_2)
 	if err != nil {
 		t.Fatalf("ERROR: failed to initialize RBAC: %s", err.Error())
 	}
 
 	// fail when typo in apiMapping
-	_, err = NewRBACAuthorizer(policyName, roleList_3, nil, apiV1Mapping_3)
+	_, err = NewRBACAuthorizer(policyName, roleList_3, apiV1Mapping_3)
 	if err == nil {
         t.Fatalf("expected an error but got nil")
     }
@@ -57,7 +57,7 @@ func TestNewRBACAuthorizer(t *testing.T) {
     }
 
 	// fail when apiV1Mapping has path not in staticAPIV1List
-	_, err = NewRBACAuthorizer(policyName, roleList_4, nil, apiV1Mapping_4)
+	_, err = NewRBACAuthorizer(policyName, roleList_4, apiV1Mapping_4)
     if err == nil {
         t.Fatal("ERROR: successfully initialized RBAC without roles")
     }
@@ -67,7 +67,7 @@ func TestNewRBACAuthorizer(t *testing.T) {
     }
 
 	// fail when apiV1Mapping has method not in staticAPIV1List
-	_, err = NewRBACAuthorizer(policyName, roleList_5, nil, apiV1Mapping_5)
+	_, err = NewRBACAuthorizer(policyName, roleList_5, apiV1Mapping_5)
 	if err == nil {
         t.Fatal("ERROR: successfully initialized RBAC without roles")
     }
