04cbffc1cb8e590982847d50e65905870d507aa6
Maia Iyer <maia.raj.iyer@gmail.com>
2024-10-21 15:22:41 -0400

    try printing individual crds
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- pkg/agent/spirecrd/crdmanager.go
+++ pkg/agent/spirecrd/crdmanager.go
@@ -8,8 +8,18 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/dynamic"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	spirev1alpha1 "github.com/spiffe/spire-controller-manager/api/v1alpha1"
 )
+
+var gvrFederation = schema.GroupVersionResource{
+	Group:    "spire.spiffe.io",
+	Version:  "v1alpha1",
+	Resource: "clusterfederatedtrustdomains",
+}
+
 
 // CRDManager defines the interface for managing CRDs
 type CRDManager interface {
@@ -20,12 +30,25 @@ type CRDManager interface {
 
 type SPIRECRDManager struct {
 	className string
+	kubeClient *dynamic.DynamicClient
 }
 
 // NewSPIRECRDManager initializes new SPIRECRDManager
 func NewSPIRECRDManager(className string) (*SPIRECRDManager, error) {
+	// assume in-cluster
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error with in-cluster config: %v", err)
+	}
+	// create client
+	kubeClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating kube client: %v", err)
+	}
+
 	return &SPIRECRDManager{
 		className: className,
+		kubeClient: kubeClient,
 	}, nil
 }
 
@@ -35,27 +58,18 @@ type ListFederationRelationshipsResponse trustdomain.ListFederationRelationships
 func (s *SPIRECRDManager) ListClusterFederatedTrustDomains(inp ListFederationRelationshipsRequest) (ListFederationRelationshipsResponse, error) { //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
 	fmt.Println("list crd federation endpoint hit")
 
-	// assume in-cluster
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return ListFederationRelationshipsResponse{}, fmt.Errorf("error with in-cluster config: %v", err)
-	}
-	// create client
-	kubeClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return ListFederationRelationshipsResponse{}, fmt.Errorf("error creating kube client: %v", err)
-	}
-
-	// define CRD's Group Version Resource
-	gvr :=schema.GroupVersionResource {
-		Group: "spire.spiffe.io",
-		Version: "v1alpha1",
-		Resource: "clusterfederatedtrustdomains",
-	}
-
-	trustDomainList, err := kubeClient.Resource(gvr).Namespace("").List(context.TODO(), metav1.ListOptions{})
+	trustDomainList, err := s.kubeClient.Resource(gvrFederation).Namespace("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return ListFederationRelationshipsResponse{}, fmt.Errorf("error listing trust domains: %v", err)
+	}
+
+	for _, trustDomain := range trustDomainList.Items {
+		var clusterFederatedTrustDomain spirev1alpha1.ClusterFederatedTrustDomain
+		err := runtime.DefaultUnstructuredConverter.FromUnstructured(trustDomain.Object, &clusterFederatedTrustDomain)
+		if err != nil {
+			return ListFederationRelationshipsResponse{}, fmt.Errorf("error parsing trustdomain: %v", err)
+		}
+		fmt.Printf("Federation CRD Name: %s, Spec: %+v\n", clusterFederatedTrustDomain.Name, clusterFederatedTrustDomain.Spec)
 	}
 
 	fmt.Printf("Listed trust domains: %v \n", trustDomainList)
