bc2e4e0295512d5972be2817987e886396e4e1c6
Maia Iyer <maia.raj.iyer@gmail.com>
2024-10-27 22:38:43 -0400

    Add GET ClusterFederatedTrustDomain API (#542)
    * removed handlers
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit spacing fixes
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Nits
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * added plugin config parsing
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Nits
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Initialize Package
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * added documentation
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit lint
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit lints
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * fix key length check
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * add list federation function to crd pkg
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * initial function handlers added
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * move types to pkg
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * initial crd list attempt
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nit case
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * try printing individual crds
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Indexing into spec
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Added parsing code and refactored
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * return result value
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Adding API documentation
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Removing print statements
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Added Documentation
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * Linted Markdown
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    * nits
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    
    ---------
    
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>
    Signed-off-by: DsysM <daienmiaoDaeM@gmail.com>

--- api/agent/config.go
+++ api/agent/config.go
@@ -100,15 +100,15 @@ func NewCRDManager(crdPlugin *ast.ObjectItem) (spirecrd.CRDManager, error) {
 	
 	// check if data is defined
 	if data == nil {
-		return "", errors.New("SPIRECRDManager plugin ('config > plugins > SPIRECRDManager > plugin_data') not populated")
+		return nil, errors.New("SPIRECRDManager plugin ('config > plugins > SPIRECRDManager > plugin_data') not populated")
 	}
 	// decode config to struct
 	var config pluginControllerManager
 	if err := hcl.DecodeObject(&config, data); err != nil {
-		return "", errors.Errorf("Couldn't parse SPIREControllerManager config: %v", err)
+		return nil, errors.Errorf("Couldn't parse SPIREControllerManager config: %v", err)
 	}
 
-	fmt.Println("CRD Controller configured. WARNING: This is currently a no-op")
+	fmt.Println("CRD Controller configured. spire-controller-manager APIs enabled")
 
 	crdManager, err := spirecrd.NewSPIRECRDManager(config.Classname)
 	if err != nil {
