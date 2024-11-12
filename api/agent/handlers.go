7d3cb4061902eb1f3a5ffc3624790bb5ce1cead8
Mason <munizm@rpi.edu>
2024-11-12 17:43:05 -0500

    Creating copyIntoBuff and updating some comments
    Signed off by: Mason Muniz <munizm@rpi.edu>

--- api/agent/handlers.go
+++ api/agent/handlers.go
@@ -22,15 +22,39 @@ TODO:
 	4. Refactor at least 3 functions before the end of the semester
 */
 
-// TODO For later: Determine if input is http.StatusBadRequest OR http.StatusInternalServerError
-// TODO: Seems like there should be a more elegant solution for this...
-// @param emsg should be "Error: " or "Error parsing data: "
+// Checks for httpError; meant to replace the (about) 4 chunks of error checking code run per function
+/* @param status can be: 
+	1 -> http.StatusBadRequest
+	2 -> http.StatusInternalServerError
+
+   @param emsg can be one of three:
+	1 -> "Error parsing data: %v", err.Error()
+	2 -> "Error: %v", err.Error()
+	3 -> "Error listing agents: %v", err.Error()
+	NOTE: Error message 1 ONLY returns with http.StatusBadRequest. Messages 2 and 3 can return with either
+*/
 func isHttpError(err error, w http.ResponseWriter, emsg string, status int) (bool){
 	if err != nil {
         retError(w, emsg + err.Error(), status);
 		return true
 	}
 	return false
+}
+
+// meant to replace the 4 lines of code repeated throughout most functions
+	// that copies the body of http.Request into buf and returns results in n and err
+/* @param emsg can be one of three:
+	1 -> "Error parsing data: %v", err.Error()
+	2 -> "Error: %v", err.Error()
+	3 -> "Error listing agents: %v", err.Error()
+*/
+func copyIntoBuff(buf *strings.Builder, w http.ResponseWriter, r *http.Request, emsg string) (int64, error, string){
+	n, err := io.Copy(buf, r.Body)
+	isErr := isHttpError(err, w, emsg + err.Error(), http.StatusBadRequest)
+	if isErr {return 0, err, "err"}
+	data := buf.String()
+
+	return n, err, data
 }
 
 // Gets called from GetRouter func in server.go
@@ -46,10 +70,10 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 	// copies body of http.Request into buf
 	// and then puts the # of bytes of type int64 into n and an error into err
 	// err is nil if there was no error
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	// n is 0 and data is "err" if err was not nil inside of copyIntoBuff
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	// if the body of the request was empty
 	if n == 0 {
@@ -66,7 +90,7 @@ func (s *Server) healthcheck(w http.ResponseWriter, r *http.Request) {
 	// ret gets the address to the HealthcheckReponse response
 		// the HealthcheckResponse is actually the response of the server "input" as returned by grpc's HealthClient.Check()
 	ret, err := s.SPIREHealthcheck(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	// Sets the headers associated with the request to specific values
@@ -113,10 +137,9 @@ func (s *Server) agentList(w http.ResponseWriter, r *http.Request) {
 	var input ListAgentsRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = ListAgentsRequest{}
@@ -127,7 +150,7 @@ func (s *Server) agentList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.ListAgents(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
 	if isErr {return}
 
 	cors(w, r)
@@ -143,10 +166,9 @@ func (s *Server) agentBan(w http.ResponseWriter, r *http.Request) {
 	var input BanAgentRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		emsg := "Error: no data provided"
@@ -159,7 +181,7 @@ func (s *Server) agentBan(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = s.BanAgent(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error listing agents: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error listing agents: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -176,10 +198,9 @@ func (s *Server) agentDelete(w http.ResponseWriter, r *http.Request) {
 	var input DeleteAgentRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		emsg := "Error: no data provided"
@@ -192,7 +213,7 @@ func (s *Server) agentDelete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = s.DeleteAgent(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error listing agents: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error listing agents: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -207,10 +228,9 @@ func (s *Server) agentCreateJoinToken(w http.ResponseWriter, r *http.Request) {
 	var input CreateJoinTokenRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = CreateJoinTokenRequest{}
@@ -221,7 +241,7 @@ func (s *Server) agentCreateJoinToken(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.CreateJoinToken(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -236,10 +256,9 @@ func (s *Server) entryList(w http.ResponseWriter, r *http.Request) {
 	var input ListEntriesRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = ListEntriesRequest{}
@@ -250,7 +269,7 @@ func (s *Server) entryList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.ListEntries(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -265,10 +284,9 @@ func (s *Server) entryCreate(w http.ResponseWriter, r *http.Request) {
 	var input BatchCreateEntryRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = BatchCreateEntryRequest{}
@@ -279,7 +297,7 @@ func (s *Server) entryCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.BatchCreateEntry(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -294,10 +312,9 @@ func (s *Server) entryDelete(w http.ResponseWriter, r *http.Request) {
 	var input BatchDeleteEntryRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = BatchDeleteEntryRequest{}
@@ -308,7 +325,7 @@ func (s *Server) entryDelete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.BatchDeleteEntry(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -323,10 +340,9 @@ func (s *Server) bundleGet(w http.ResponseWriter, r *http.Request) {
 	var input GetBundleRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = GetBundleRequest{}
@@ -337,7 +353,7 @@ func (s *Server) bundleGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.GetBundle(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -351,10 +367,9 @@ func (s *Server) federatedBundleList(w http.ResponseWriter, r *http.Request) {
 	var input ListFederatedBundlesRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = ListFederatedBundlesRequest{}
@@ -365,7 +380,7 @@ func (s *Server) federatedBundleList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.ListFederatedBundles(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -379,10 +394,9 @@ func (s *Server) federatedBundleCreate(w http.ResponseWriter, r *http.Request) {
 	var input CreateFederatedBundleRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = CreateFederatedBundleRequest{}
@@ -393,7 +407,7 @@ func (s *Server) federatedBundleCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.CreateFederatedBundle(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -407,10 +421,9 @@ func (s *Server) federatedBundleUpdate(w http.ResponseWriter, r *http.Request) {
 	var input UpdateFederatedBundleRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
 	if n == 0 {
 		input = UpdateFederatedBundleRequest{}
@@ -421,7 +434,7 @@ func (s *Server) federatedBundleUpdate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.UpdateFederatedBundle(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -435,11 +448,9 @@ func (s *Server) federatedBundleDelete(w http.ResponseWriter, r *http.Request) {
 	var input DeleteFederatedBundleRequest
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-	data := buf.String()
-
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 	if n == 0 {
 		input = DeleteFederatedBundleRequest{}
 	} else {
@@ -449,7 +460,7 @@ func (s *Server) federatedBundleDelete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.DeleteFederatedBundle(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusInternalServerError)
 	if isErr {return}
 
 	cors(w, r)
@@ -485,11 +496,10 @@ func (s *Server) health(w http.ResponseWriter, r *http.Request) {
 func (s *Server) tornjakSelectorsList(w http.ResponseWriter, r *http.Request) {
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
-	data := buf.String()
 	var input ListSelectorsRequest
 
 	if n == 0 {
@@ -501,7 +511,7 @@ func (s *Server) tornjakSelectorsList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.ListSelectors(input)
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
 	if isErr {return}
 
 	cors(w, r)
@@ -515,11 +525,10 @@ func (s *Server) tornjakSelectorsList(w http.ResponseWriter, r *http.Request) {
 func (s *Server) tornjakPluginDefine(w http.ResponseWriter, r *http.Request) {
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
 
-	data := buf.String()
 	var input RegisterSelectorRequest
 
 	if n == 0 {
@@ -531,7 +540,7 @@ func (s *Server) tornjakPluginDefine(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = s.DefineSelectors(input)
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
 	if isErr {return}
 
 	cors(w, r)
@@ -544,11 +553,10 @@ func (s *Server) tornjakPluginDefine(w http.ResponseWriter, r *http.Request) {
 func (s *Server) tornjakAgentsList(w http.ResponseWriter, r *http.Request) {
 	buf := new(strings.Builder)
 
-	n, err := io.Copy(buf, r.Body)
-	isErr := isHttpError(err, w, fmt.Sprintf("Error parsing data: %v", err.Error()), http.StatusBadRequest)
-	if isErr {return}
-
-	data := buf.String()
+	emsg := "Error parsing data: "
+	n, err, data := copyIntoBuff(buf, w, r, emsg)
+	if n == 0 && data == "err" { return }
+	
 	var input ListAgentMetadataRequest
 
 	if n == 0 {
@@ -560,7 +568,7 @@ func (s *Server) tornjakAgentsList(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ret, err := s.ListAgentMetadata(input)
-	isErr = isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
+	isErr := isHttpError(err, w, fmt.Sprintf("Error: %v", err.Error()), http.StatusBadRequest)
 	if isErr {return}
 
 	cors(w, r)
