474cf372c8abcc64b7b7c724dca605e48256e6dd
rspacerr <definitelynotoberon@gmail.com>
2024-11-12 17:00:06 -0500

    Refactor CRDFederationList to use error function
    Signed-off-by: rspacerr <definitelynotoberon@gmail.com>

--- api/agent/crd_handlers.go
+++ api/agent/crd_handlers.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"strings"
@@ -22,9 +21,7 @@ func (s *Server) CRDFederationList(w http.ResponseWriter, r *http.Request) {
 	buf := new(strings.Builder)
 
 	n, err := io.Copy(buf, r.Body)
-	if err != nil {
-		emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
+	if isHttpError(err, w, "Error parsing data: ", http.StatusBadRequest) {
 		return
 	}
 	data := buf.String()
@@ -33,27 +30,20 @@ func (s *Server) CRDFederationList(w http.ResponseWriter, r *http.Request) {
 		input = crdmanager.ListFederationRelationshipsRequest{}
 	} else {
 		err := json.Unmarshal([]byte(data), &input)
-		if err != nil {
-			emsg := fmt.Sprintf("Error parsing data: %v", err.Error())
-			retError(w, emsg, http.StatusBadRequest)
+		if isHttpError(err, w, "Error parsing data: ", http.StatusBadRequest) {
 			return
 		}
 	}
 
 	ret, err := s.CRDManager.ListClusterFederatedTrustDomains(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusInternalServerError)
+	if isHttpError(err, w, "Error: ", http.StatusInternalServerError) {
 		return
 	}
 
 	cors(w, r)
 	je := json.NewEncoder(w)
 	err = je.Encode(ret) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
-	if err != nil {
-		emsg := fmt.Sprintf("Error: %v", err.Error())
-		retError(w, emsg, http.StatusBadRequest)
+	if isHttpError(err, w, "Error: ", http.StatusBadRequest) {
 		return
 	}
-
 }
