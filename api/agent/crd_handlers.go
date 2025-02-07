53d4281c0b5fa8d9077bf21bbe1f8cb8fe10ba57
Mason Muniz <munizm@rpi.edu>
2025-02-07 17:06:16 -0500

    Bringing crd_handlers.go up to date with Maia's 34cf316 commit
    Signed-off-by: Mason Muniz <munizm@rpi.edu>

--- api/agent/crd_handlers.go
+++ api/agent/crd_handlers.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	crdmanager "github.com/spiffe/tornjak/pkg/agent/spirecrd"
+	trustdomain "github.com/spiffe/spire-api-sdk/proto/spire/api/server/trustdomain/v1"
 )
 
 func (s *Server) CRDFederationList(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +41,45 @@ func (s *Server) CRDFederationList(w http.ResponseWriter, r *http.Request) {
 	cors(w, r)
 	je := json.NewEncoder(w)
 	err = je.Encode(ret) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
+	
+	if isHttpError(err, w, "Error: ", http.StatusBadRequest) {
+		return
+	}
+}
+
+func (s *Server) CRDFederationCreate(w http.ResponseWriter, r *http.Request) {
+	if s.CRDManager == nil {
+		emsg := "Error: CRD Manager not configured on Tornjak."
+		retError(w, emsg, http.StatusBadRequest)
+		return
+	}
+	var input crdmanager.BatchCreateFederationRelationshipsRequest
+	var rawInput trustdomain.BatchCreateFederationRelationshipRequest
+	buf := new(strings.Builder)
+
+	n, err, data := copyIntoBuff(buf, w, r, "Error parsing data: ")
+	if n == 0 && data == "err" { return }
+
+	if n == 0 {
+		input = crdmanager.BatchCreateFederationRelationshipsRequest{}
+	} else {
+		// required to use protojson because of oneof field
+		err := protojson.Unmarshal([]byte(data), &rawInput)
+		if isHttpError(err, w, "Error parsing data: ", http.StatusBadRequest) {
+			return
+		}
+		input = crdmanager.BatchCreateFederationRelationshipsRequest(rawInput) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
+	}
+
+	ret, err := s.CRDManager.BatchCreateClusterFederatedTrustDomains(input) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
+	if isHttpError(err, w, "Error: ", http.StatusInternalServerError) {
+		return
+	}
+
+	cors(w, r)
+	je := json.NewEncoder(w)
+	err = je.Encode(ret) //nolint:govet //Ignoring mutex (not being used) - sync.Mutex by value is unused for linter govet
+
 	if isHttpError(err, w, "Error: ", http.StatusBadRequest) {
 		return
 	}
