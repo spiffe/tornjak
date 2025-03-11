4ccb97ca68401e54d11a0b8738159fa51a07defd
zhouh11 <zhouh11@rpi.edu>
2025-03-11 17:10:07 -0400

    edit
    Signed-off-by: zhouh11 <zhouh11@rpi.edu>

--- frontend/src/components/dashboard/dashboard-details-render.tsx
+++ frontend/src/components/dashboard/dashboard-details-render.tsx
@@ -4,8 +4,7 @@ import IsManager from '../is_manager';
 import DashboardDetailsStyled from './dashboard-details';
 import TornjakHelper from '../tornjak-helper';
 import TornjakApi from '../tornjak-api-helpers';
-
-
+import { RootState } from 'redux/reducers';
 import {
     serverSelectedFunc,
     clustersListUpdateFunc,
@@ -19,20 +18,61 @@ import {
     agentworkloadSelectorInfoFunc,
     clusterTypeInfoFunc,
     clickedDashboardTableFunc,
-    
 } from 'redux/actions';
 
-import{
-    DebugServerInfo,
-
-
+import {
+    AgentsList,
+    AgentsWorkLoadAttestorInfo,
+    ClustersList,
+    ServersList,
+    EntriesList,
+    SelectorInfoLabels,
+    ServerInfo,
+    TornjakServerInfo,
+    WorkloadSelectorInfoLabels,
+    SpireHealthCheckFreq,
+    DebugServerInfo, FederationsList
 } from "components/types";
 
+
+
 type DashboardDetailsRenderProps ={
-    globalClustersList: Array<{ [key: string]: any }>;
-    globalDebugServerInfo:DebugServerInfo ;
-    globalServerSelected:string;
-    clickedDashboardTableFunc: (globalClickedDashboardTable: string) => void;
+    // dispatches a payload for the server selected and has a return type of void
+    serverSelectedFunc: (globalServerSelected: string) => void,
+    // dispatches a payload for list of clusters with their metadata info as an array of ClustersList Type and has a return type of void
+    clustersListUpdateFunc: (globalClustersList: ClustersList[]) => void,
+    // dispatches a payload for list of agents with their metadata info as an array of AgentListType and has a return type of void
+    agentsListUpdateFunc: (globalAgentsList: AgentsList[]) => void,  
+    // dispatches a payload for list of entries with their metadata info as an array of EntriesListType and has a return type of void
+    entriesListUpdateFunc: (globalEntriesList: EntriesList[]) => void,
+    // dispatches a payload for the tornjak server info of the selected server and has a return type of void
+    tornjakServerInfoUpdateFunc: (globalTornjakServerInfo: TornjakServerInfo) => void, 
+    // dispatches a payload for the server trust domain and nodeAttestorPlugin and has a return type of void
+    serverInfoUpdateFunc: (globalServerInfo: ServerInfo) => void,
+    // dispatches a payload for list of available selectors and their options as an object and has a return type of void
+    selectorInfoFunc: (globalSelectorInfo: SelectorInfoLabels) => void,
+    // dispatches a payload for an Error Message/ Success Message of an executed function as a string and has a return type of void
+    tornjakMessageFunc: (globalErrorMessage: string) => void,
+    // dispatches a payload for list of available workload selectors and their options as an object and has a return type of void
+    workloadSelectorInfoFunc: (globalWorkloadSelectorInfo: WorkloadSelectorInfoLabels) => void, 
+    // dispatches a payload for the workload selector info for the agents as an array of AgentsWorkLoadAttestorInfoType and has a return type of void
+    agentworkloadSelectorInfoFunc: (globalAgentsWorkLoadAttestorInfo: AgentsWorkLoadAttestorInfo[]) => void,  
+    // dispatches a payload for list of available cluster types as array of strings and has a return type of void
+    clusterTypeInfoFunc: (globalClusterTypeInfo: string[]) => void,
+    // dispatches a payload for the clicked table in a dashboard as a string and has a return type of void
+    clickedDashboardTableFunc: (globalClickedDashboardTable: string) => void,
+    // the selected server for manager mode 
+    globalServerSelected: string, 
+    // tornjak server debug info of the selected server
+    globalDebugServerInfo: DebugServerInfo,
+    // tornjak server info of the selected server
+    globalTornjakServerInfo: TornjakServerInfo, 
+
+  
+    
+    
+
+
 }
 // DashboardDetailsRender takes in details url parameters
 // returns details page for dashboard for a specific entity
@@ -48,7 +88,7 @@ class DashboardDetailsRender extends React.Component<DashboardDetailsRenderProps
 
     componentDidMount() {
         const { params } = this.props;
-        if (this.props.globalDebugServerInfo === "") {
+        if (this.props.globalDebugServerInfo === undefined) {
             if (IsManager) {
                 if (this.props.globalServerSelected !== "") {
                     this.props.clickedDashboardTableFunc(params.entity + "details");
@@ -67,7 +107,7 @@ class DashboardDetailsRender extends React.Component<DashboardDetailsRenderProps
                 this.TornjakApi.populateLocalEntriesUpdate(this.props.entriesListUpdateFunc, this.props.tornjakMessageFunc);
                 this.TornjakApi.populateLocalTornjakAgentInfo(this.props.agentworkloadSelectorInfoFunc, "");
                 this.TornjakApi.populateLocalTornjakServerInfo(this.props.tornjakServerInfoUpdateFunc, this.props.tornjakMessageFunc);
-                if (this.props.globalTornjakServerInfo !== "") {
+                if (this.props.globalTornjakServerInfo !== null) {
                     this.TornjakApi.populateServerInfo(this.props.globalTornjakServerInfo, this.props.serverInfoUpdateFunc);
                 }
             }
@@ -82,7 +122,7 @@ class DashboardDetailsRender extends React.Component<DashboardDetailsRenderProps
     }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: RootState) => ({
     globalServerInfo: state.servers.globalServerInfo,
     globalTornjakServerInfo: state.servers.globalTornjakServerInfo,
     globalClustersList: state.clusters.globalClustersList,
